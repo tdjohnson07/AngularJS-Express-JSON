4cb2f7b04ece5a49a712d19e7d2f2ef40b28b645
Trent Johnson <tdjohnson0722@gmail.com>
2016-07-12 14:34:50 -0500

    finished assignment

---
+++ routes/index.js
@@ -0,0 +1,6 @@
+var router = require('express').Router();
+var path = require ('path');
+router.get('/', function(request, response){
+  response.sendFile(path.join(__dirname, '../public/index.html'));
+});
+module.exports= router;
